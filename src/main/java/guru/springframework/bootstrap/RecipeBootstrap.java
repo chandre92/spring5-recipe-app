bdecf9b600ec430e99380d216d7b9b8f978d0d6c
chandre <disable1992@gmail.com>
2021-05-25 22:37:38 +0300

    Implemented lombok usage assigment

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
@@ -30,6 +32,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     private List<Recipe> getRecipes() {
+        log.debug("Getting some recipes");
         List<Recipe> recipes = new ArrayList<>();
 
         UnitOfMeasure eachUom = unitOfMeasureRepository.findByDescription("Each").orElseThrow();
