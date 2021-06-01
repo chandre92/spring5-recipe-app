064b546be0b4391036aa71a963de2e72dce3fa1c
chandre <disable1992@gmail.com>
2021-06-01 22:55:08 +0300

    Improved IndexController unit tests

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,12 +1,18 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
+
+import java.util.Collections;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -22,15 +28,23 @@ class IndexControllerTest {
     @Mock
     RecipeService recipeService;
 
+    @Captor
+    ArgumentCaptor<Set<Recipe>> recipesCaptor;
+
     @Test
     void getIndexPage() {
+        // Arrange
+        Set<Recipe> recipes = Collections.singleton(new Recipe());
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
         // Act
         String indexPage = indexController.getIndexPage(model);
 
         // Assert
         assertThat(indexPage).isEqualTo("index");
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), recipesCaptor.capture());
         verify(recipeService, times(1)).getRecipes();
         verifyNoMoreInteractions(model, recipeService);
+        assertThat(recipesCaptor.getValue()).isEqualTo(recipes);
     }
 }
